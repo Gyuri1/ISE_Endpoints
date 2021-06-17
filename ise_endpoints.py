18bf11170fb7f523e019b37465adb8ee39d8617c
Gyuri1 <Acs.gyorgy1@gmail.com>
2021-06-17 16:24:36 +0200

    Add files via upload
    With optional profiling_info

--- ise_endpoints.py
+++ ise_endpoints.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import urllib3
+import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -8,12 +9,16 @@ ISE_hostname="ise.local"
 ISE_admin="admin"
 ISE_password="secret"
 
+
 total=0
 payload={}
 headers = {
   'Content-Type': 'application/json',
   'Accept': 'application/json'
 }
+
+baseurl = "https://"+ISE_hostname +":9060/ers/config/endpoint"
+ 
 
 def GetProfileName(EndpointID):
   try:
@@ -36,43 +41,85 @@ def GetProfileName(EndpointID):
     response.raise_for_status()
   except requests.exceptions.HTTPError as err:
     raise SystemExit(err)
-  #print(response.text)
   res2=json.loads(response.text)
-  name=res2["ProfilerProfile"]["name"]
-  #print(res2["ProfilerProfile"]["name"])
+  try:
+    name=res2["ProfilerProfile"]["name"]
+  except:
+    name="No_profilename_yet"
   return name
 
-
-dev=0
-print("ISE Endpoints:")
-baseurl = "https://"+ISE_hostname +":9060/ers/config/endpoint"
-try:
-  response = requests.get(baseurl, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
-  response.raise_for_status()
-except requests.exceptions.HTTPError as err:
-  raise SystemExit(err)
-
-
-res=json.loads(response.text)
-total=res["SearchResult"]["total"]
-print("Total number of endpoints:",total)
-
-#print("Endpoints:",response.text)
-
-for device in res["SearchResult"]["resources"]:
-  dev=dev+1
-  print(dev,":",device["name"], GetProfileName( device["link"]["href"] ))
-
-while "nextPage" in res["SearchResult"] :
-  # use the next page reference after we've retrieved the first page    
-  url = res["SearchResult"]["nextPage"]["href"]
+def List_ISE_Endpoints_with_Profile():
+  dev=0
   try:
-    response = requests.get(url, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
+    response = requests.get(baseurl, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
     response.raise_for_status()
   except requests.exceptions.HTTPError as err:
     raise SystemExit(err)
 
-  res = json.loads(response.text)
+  res=json.loads(response.text)
+  total=res["SearchResult"]["total"]
+  print("Total number of endpoints:",total)
+
   for device in res["SearchResult"]["resources"]:
     dev=dev+1
     print(dev,":",device["name"], GetProfileName( device["link"]["href"] ))
+
+  while "nextPage" in res["SearchResult"] :
+  # use the next page reference after we've retrieved the first page    
+    url = res["SearchResult"]["nextPage"]["href"]
+    try:
+      response = requests.get(url, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
+      response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+      raise SystemExit(err)
+
+    res = json.loads(response.text)
+    for device in res["SearchResult"]["resources"]:
+      dev=dev+1
+      print(dev,":",device["name"], GetProfileName( device["link"]["href"] ))
+
+def List_ISE_Endpoints():
+  dev=0
+  try:
+    response = requests.get(baseurl, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
+    response.raise_for_status()
+  except requests.exceptions.HTTPError as err:
+    raise SystemExit(err)
+
+  res=json.loads(response.text)
+  total=res["SearchResult"]["total"]
+  print("Total number of endpoints:",total)
+
+  for device in res["SearchResult"]["resources"]:
+    dev=dev+1
+    print(dev,":",device["name"])
+
+  while "nextPage" in res["SearchResult"] :
+  # use the next page reference after we've retrieved the first page    
+    url = res["SearchResult"]["nextPage"]["href"]
+    try:
+      response = requests.get(url, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
+      response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+      raise SystemExit(err)
+
+    res = json.loads(response.text)
+    for device in res["SearchResult"]["resources"]:
+      dev=dev+1
+      print(dev,":",device["name"])
+
+
+# main 
+if __name__ == "__main__":
+    print("List ISE Endpoints:")
+    if len(sys.argv) == 2:
+      if str(sys.argv[1]) == "profiling_info=true":
+        List_ISE_Endpoints_with_Profile()
+        exit()
+      else:
+        print("HOW TO USE:")
+        print("python3 ise_endpoints.py [profiling_info=true]" )
+        exit()
+    List_ISE_Endpoints()
+
+
