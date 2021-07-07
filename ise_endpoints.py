77cd576ff7c014cdacbade1d1f287e0376c9d731
Gyuri1 <Acs.gyorgy1@gmail.com>
2021-07-07 22:28:48 +0200

    Add files via upload

--- ise_endpoints.py
+++ ise_endpoints.py
@@ -1,3 +1,4 @@
+
 import requests
 import json
 import urllib3
@@ -5,9 +6,16 @@ import sys, csv
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-ISE_hostname="10.10.20.70"
+ISE_hostname="ise"
 ISE_admin="apiadmin"
-ISE_password="C1sco123"
+ISE_password="secret"
+
+csvfilename = None
+profilermode = False
+
+header = ['index', 'MAC_address' ]
+headerWithProfiler = ['index', 'MAC_address', 'Profile_Name' ]
+
 
 total=0
 payload={}
@@ -44,19 +52,37 @@ def GetProfileName(EndpointID):
     name="No_profilename_yet"
   return name
 
-def List_ISE_Endpoints_with_Profile():
+def List_ISE_Endpoints_with_Profile(csvfilename = None):
+
   dev=0
   try:
     response = requests.get(baseurl, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
     response.raise_for_status()
   except Exception as err:
-    raise RuntimeError(exception_string(err)) from err 
+    print('Error: ISE is not available')
   res=json.loads(response.text)
   total=res["SearchResult"]["total"]
   #print("Total number of endpoints:",total)
+
+
+  if csvfilename: 
+    # open the file in the write mode
+    f = open(csvfilename, 'w')
+    # create the csv writer
+    writer = csv.writer(f) 
+    # write a row to the csv file
+
+  if csvfilename:  
+    writer.writerow(headerWithProfiler)
   print("index,MAC_address,Profile_Name")
+
+  
   for device in res["SearchResult"]["resources"]:
     dev=dev+1
+
+    if csvfilename:  
+      # write a row to the csv file
+      writer.writerow( [ dev , device['name'], GetProfileName( device['link']['href']) ]) 
     print(f"{dev},{device['name']},{GetProfileName( device['link']['href'])}")
       
   while "nextPage" in res["SearchResult"] :
@@ -66,51 +92,108 @@ def List_ISE_Endpoints_with_Profile():
       response = requests.get(url, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
       response.raise_for_status()
     except Exception as err:
-      raise RuntimeError(exception_string(err)) from err 
+      print('Error: wrong ISE request') 
     res = json.loads(response.text)
     for device in res["SearchResult"]["resources"]:
       dev=dev+1
+
+      if csvfilename:  
+        # write a row to the csv file
+        writer.writerow( [ dev , device['name'], GetProfileName( device['link']['href']) ])
+
       print(f"{dev},{device['name']},{GetProfileName( device['link']['href'])}")
 
+  if csvfilename:  
+    # close the file
+    f.close()    
+
+
 def List_ISE_Endpoints(filename=None):
+
   dev=0
   baseurl = "https://"+ISE_hostname +":9060/ers/config/endpoint"
   try:
     response = requests.get(baseurl, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
     response.raise_for_status()
   except Exception as err:
-    raise RuntimeError(exception_string(err)) from err
+    print('Error: wrong ISE request')
   res=json.loads(response.text)
   total=res["SearchResult"]["total"]
   #print("Total number of endpoints:",total)
+
+  if csvfilename: 
+    # open the file in the write mode
+    f = open(csvfilename, 'w')
+    # create the csv writer
+    writer = csv.writer(f) 
+    # write a row to the csv file
+
+  if csvfilename:  
+    writer.writerow(header)
+  
   print("index,MAC_address")
+
   for device in res["SearchResult"]["resources"]:
     dev=dev+1
-    print(f"{dev},{device['name']}")
+    
+    if csvfilename:  
+      # write a row to the csv file
+      writer.writerow( [ dev , device['name'] ])
+    
+    print(f"{dev},{device['name']}") 
   
   while "nextPage" in res["SearchResult"] :
-  # use the next page reference after we've retrieved the first page    
+    # use the next page reference after we've retrieved the first page    
     url = res["SearchResult"]["nextPage"]["href"]
     try:
       response = requests.get(url, headers=headers, data=payload, verify=False, auth=(ISE_admin, ISE_password))
       response.raise_for_status()
     except Exception as err:
-      raise RuntimeError(exception_string(err)) from err
+      print('Error: wrong ISE request')
     res = json.loads(response.text)
     for device in res["SearchResult"]["resources"]:
       dev=dev+1
-      print(f"{dev},{device['name']}")
 
-# main 
+      if csvfilename:  
+        # write a row to the csv file
+        writer.writerow([ dev , device['name'] ])
+  
+      print(f"{dev},{device['name']}" ) 
+
+  if csvfilename:  
+    # close the file
+    f.close()
+
+def SmartExit(outstring):
+   print(f"Not supported option:{outstring}")
+   print("HOW TO USE:")
+   print("python3 ise_endpoints.py [profiling_info=true] [csvfilename=output.csv]" )
+   sys.exit("Wrong argument")
+
+#
+# main
+#
 if __name__ == "__main__":
-    #print("List ISE Endpoints:")
-    if len(sys.argv) == 2:
-      if str(sys.argv[1]) == "profiling_info=true":
-        List_ISE_Endpoints_with_Profile()
+ 
+  for i in sys.argv:
+    if i=="ise_endpoints.py": 
+      continue
+
+    split_string = i.split("=", 1)
+    if split_string[0] == "profiling_info":
+      if split_string[1] == 'true':
+        profilermode = True
+      elif profilermode == "false":
+        profilermode = False
       else:
-        print("HOW TO USE:")
-        print("python3 ise_endpoints.py [profiling_info=true]" )
+        SmartExit(i)
+
+    elif split_string[0] == "csvfilename":
+      csvfilename = split_string[1]
     else:
-      List_ISE_Endpoints()
+      SmartExit(i)
 
-
+  if profilermode:
+    List_ISE_Endpoints_with_Profile(csvfilename)
+  else:  
+    List_ISE_Endpoints(csvfilename)
